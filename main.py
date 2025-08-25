8660be2e3547395812c3e6e3e50495c338ce2572
Pawelo112 <elopawelo112@gmail.com>
2025-08-25 18:46:20 +0200

    Create updating mechanism of list of words to learn

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-
 # ---------------------------- VARIABLES ------------------------------- #
-words_data = pd.read_csv("data/french_words.csv")
+# Checking if learning file exist
+# if not - read from main file
+try:
+    words_data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    words_data = pd.read_csv("data/french_words.csv")
 
 # Transforming DataFrame to a list of dictionaries
 words_to_learn = words_data.to_dict(orient='records')
@@ -26,6 +30,20 @@ def generate_card():
     canvas.itemconfig(title, text='French', fill='black')
     canvas.itemconfig(main_image, image=card_front_image)
     side = 'front'
+
+
+def generate_card_and_update_words():
+    """Generates and displays new flash card.
+    Additionally, removes current word from learning list."""
+    global word, side
+
+    # Removing learned word from a list
+    words_to_learn.remove(word)
+    updated_words_data = pd.DataFrame(words_to_learn)
+    updated_words_data.to_csv('data/words_to_learn.csv', index=False)
+
+    # Generating new card with updated list of words
+    generate_card()
 
 
 def flip_card():
@@ -63,12 +81,13 @@ canvas.grid(row=0, column=0, columnspan=3)
 
 # Buttons
 correct_image = PhotoImage(file='images/right.png')
-correct_button = Button(image=correct_image, highlightthickness=0, compound='center', bd=0, command=generate_card)
+correct_button = Button(image=correct_image, highlightthickness=0, compound='center',
+                        bd=0, command=generate_card_and_update_words)
 correct_button.grid(row=1, column=2)
 
 flip_image = PhotoImage(file='images/flip.png')
-flip_button = Button(image=flip_image, background=BACKGROUND_COLOR, highlightthickness=0, compound='center', bd=0,  command=flip_card)
-
+flip_button = Button(image=flip_image, background=BACKGROUND_COLOR, highlightthickness=0, compound='center',
+                     bd=0,  command=flip_card)
 flip_button.grid(row=1, column=1)
 
 wrong_image = PhotoImage(file='images/wrong.png')
