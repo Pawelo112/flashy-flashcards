7c6e47f26cc96422ad5370f315073a1867e439a9
Pawelo112 <elopawelo112@gmail.com>
2025-08-25 11:04:56 +0200

    Create text on flash card.

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526, highlightthickness=0)
 flash_card_image = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=flash_card_image)
+# Flash Card text
+canvas.create_text(400, 150, text='French', font=('Ariel', 40, 'italic'))
+canvas.create_text(400, 263, text='trouve', font=('Ariel', 60, 'bold'))
 canvas.grid(row=0, column=0, columnspan=1)
+
+# Buttons
+
 
 window.mainloop()
